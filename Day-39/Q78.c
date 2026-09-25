2a4bed20754087f01d2703f911a48336588a8b4a
Arjun Shukla <arjunshuklacse@gmail.com>
2026-09-25 16:54:50 +0530

    Day 39: Q77, Q78

---
+++ Day-39/Q78.c
@@ -0,0 +1,35 @@
+/*
+Q78: Find the sum of main diagonal elements for a square matrix.
+
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 1:
+15
+*/
+
+#include <stdio.h>
+
+int main() {
+    int r, c, a[10][10], sum = 0;
+    scanf("%d %d", &r, &c);
+    if (r < 1 || r > 10 || c < 1 || c > 10) {
+        printf("Invalid size\n");
+        return 0;
+    }
+    if (r != c) {
+        printf("Not a square matrix\n");
+        return 0;
+    }
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+    for (int i = 0; i < r; i++) {
+        sum += a[i][i];
+    }
+    printf("%d\n", sum);
+    return 0;
+}
